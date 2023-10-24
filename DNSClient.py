84c500df6750165a0add161ba118491b7dea5056
shivanigandhi8013 <sg8013@nyu.edu>
2023-10-24 13:00:42 -0400

    1

--- DNSClient.py
+++ DNSClient.py
@@ -22,10 +22,12 @@ def query_local_dns_server(domain, question_type):
 # Define a function to query a public DNS server for the IP address of a given domain name
 def query_dns_server(domain, question_type):
     resolver = dns.resolver.Resolver()
-    resolver.nameservers = [????]
+    resolver.nameservers = []
+    #resolver.nameservers = [????]
     answers = resolver.resolve(domain, question_type)  # provide the domain and question_type
 
-    ip_address = answers[??].to_text()
+    ip_address = answers[].to_text()
+    #ip_address = answers[??].to_text()
     return ip_address
 
 
